9afd7ef5ee1c94b20446688f6fb574825ab4af05
Rahul Kathuria <rahulkathuria008@gmail.com>
2023-05-10 01:43:30 +0530

    flex

--- src/components/ExploreCard.jsx
+++ src/components/ExploreCard.jsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import styles from '../styles/styles';
 import { fadeIn } from '../utils/motion';
 
 const ExploreCard = ({ id, imgUrl, img, title, desc, prompt, index, active, handleClick }) => (
@@ -12,30 +11,37 @@ const ExploreCard = ({ id, imgUrl, img, title, desc, prompt, index, active, hand
     } flex items-center justify-center min-w-[250px] h-[300px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
     onClick={() => handleClick(id)}
   >
+   
     <img
       src={imgUrl}
       alt="planet-04"
       className="absolute w-full h-full object-cover "
     />
+    
     {/* <p className='text-white'>{desc}</p> */}
     {active !== id ? (
       <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-30 lg:rotate-[-90deg] lg:origin-[0,0]">
         {title}
       </h3>
     ) : (
-      <div dir='rtl' className="absolute bottom-0 p-8 flex justify-start w-full flex-col rounded-b-[24px]"   >
+      
+      <div dir='rtl' className="absolute bottom-0 p-8 flex justify-start w-full flex-col rounded-b-[24px]" >
+        <h2 className="mr-[250px] font-semibold sm:text-[32px] text-[24px] text-white">
+          {title}
+        </h2>
         <div dir='rtl'
           className={`flex items-center inset-y-0 right-0 w-[120px] h-[120px] rounded-[24px] glassmorphism mb-[16px]`}
         >
+          
           <img
             src={img}
             alt="headset"
-            className="object-contain"
+            className="object-contain mb-[35px]"
           />
         </div>
-        <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">
+        {/* <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">
           {title}
-        </h2>
+        </h2> */}
         <p className='text-white text-left'>{desc}</p>
         <p className="font-normal text-[16px] leading-[20.16px] text-white ">
           {prompt}
